f8613997d92d4d200eaaf4859062f3f9bea7b138
Joonas Kylmälä <joonas.kylmala@iki.fi>
2020-07-07 18:13:15 -0400

    Add android.hardware.power to manifest
    This is needed by BatteryStatsService
    
    Signed-off-by: Joonas Kylmälä <joonas.kylmala@iki.fi>

--- midas.mk
+++ midas.mk
@@ -114,6 +114,7 @@ PRODUCT_PACKAGES += \
 # Power HAL packages
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service \
 
 # Memory HAL packages
 PRODUCT_PACKAGES += \
