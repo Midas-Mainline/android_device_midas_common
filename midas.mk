db781db4ac45e38af1cbdd8f59e8c3f96e86d62b
Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
2020-11-27 17:50:13 +0100

    midas.mk: graphics: remove duplicated PRODUCT_PACKAGES
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>

--- midas.mk
+++ midas.mk
@@ -30,14 +30,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
-PRODUCT_PACKAGES += \
-    gralloc.gbm \
-    hwcomposer.ranchu \
-    libEGL_swiftshader \
-    libGLESv2_swiftshader \
-    android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.composer@2.1-service \
-
 # HAL packages
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
