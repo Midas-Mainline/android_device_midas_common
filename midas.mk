ba0e908471eff4db76719ba621f58b74f0a3720f
Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
2020-11-27 17:51:06 +0100

    midas.mk: graphics: order PRODUCT_PACKAGES alphabetically
    This is to avoid accidental duplication of entries.
    
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>

--- midas.mk
+++ midas.mk
@@ -20,22 +20,19 @@
 PRODUCT_PACKAGES += \
     gralloc.gbm \
     hwcomposer.drm \
-    libGLES_mesa \
     hwcomposer.ranchu \
     libEGL_swiftshader \
+    libGLES_mesa \
     libGLESv2_swiftshader \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.renderscript@1.0-impl \
 
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
-# HAL packages
-PRODUCT_PACKAGES += \
-    android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.renderscript@1.0-impl \
 
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/allocator/android.hardware.graphics.allocator@2.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.graphics.allocator@2.0-override-service.rc
 
