f71c064201c32ab1753f3cfe238db6a12b6b8aaf
Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
2020-11-27 17:59:22 +0100

    midas.mk: sort sections alphabetically
    This is to avoid potential duplication of sections.
    
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>

--- midas.mk
+++ midas.mk
@@ -14,70 +14,17 @@
 # limitations under the License.
 #
 
-############
-# Graphics #
-############
-PRODUCT_PACKAGES += \
-    gralloc.gbm \
-    hwcomposer.drm \
-    hwcomposer.ranchu \
-    libEGL_swiftshader \
-    libGLES_mesa \
-    libGLESv2_swiftshader \
-    android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.renderscript@1.0-impl \
-
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/allocator/android.hardware.graphics.allocator@2.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.graphics.allocator@2.0-override-service.rc
-
-##########
-# Lights #
-##########
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.samsung \
-
-#######
-# USB #
-#######
-
-# HAL packages
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
-
-# Enable Hardware compoments on the framework level
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
-
-##########
-# Health #
-##########
-PRODUCT_PACKAGES += \
-	android.hardware.health@2.1-service \
-	android.hardware.health@2.1-impl \
-
-#########
-# Power #
-#########
-
-# HAL packages
-PRODUCT_PACKAGES += \
-	android.hardware.power-service.example \
-
-########
-# VNDK #
-########
-PRODUCT_PACKAGES += \
-    vndk_package
+###############
+# ADB support #
+###############
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.service.adb.enable=1 \
+    persist.sys.usb.config=adb
 
 #########
 # Audio #
 #########
+
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio@6.0-impl \
@@ -102,19 +49,15 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/surround_sound_configuration_5_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/surround_sound_configuration_5_0.xml
 
-##########
-# Kernel #
-##########
-PRODUCT_COPY_FILES += \
-	kernel/replicant/linux/zImage-dtb:kernel \
+#########
+# Debug #
+#########
 
-###########################
-# unsupported HW features #
-###########################
-
-# Copy list of unsupported HW features
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/unsupported_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/unsupported_hardware.xml
+# Get root on the serial console for -eng builds
+# This can help debugging early boot issues
+ifeq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/console.rc:system/etc/init/console.rc
+endif
 
 ########################################
 # DRM (Digital Restriction Management) #
@@ -126,21 +69,65 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
 
 ############
-# Security #
-############
-# Keymaster HAL packages
-PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-service.software \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service \
-
-############
-# Vibrator #
+# Graphics #
 ############
 
-# HAL packages
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
+    gralloc.gbm \
+    hwcomposer.drm \
+    hwcomposer.ranchu \
+    libEGL_swiftshader \
+    libGLES_mesa \
+    libGLESv2_swiftshader \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.renderscript@1.0-impl \
+
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/allocator/android.hardware.graphics.allocator@2.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.graphics.allocator@2.0-override-service.rc
+
+#########
+# Hacks #
+#########
+
+# HACK: prevent the device to go in suspend because it's annoying during early
+# development. Remove afterward as it consume way more energy this way.
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/prevent_suspend.sh:system/bin/prevent_suspend.sh
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/prevent_suspend.rc:system/etc/init/prevent_suspend.rc
+
+##########
+# Health #
+##########
+
+PRODUCT_PACKAGES += \
+	android.hardware.health@2.1-service \
+	android.hardware.health@2.1-impl \
+
+####################
+# Heimdall support #
+####################
+
+# Enable flashing through heimdall
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/resize2fs_partitions.sh:system/bin/resize2fs_partitions.sh
+
+##########
+# Kernel #
+##########
+
+PRODUCT_COPY_FILES += \
+	kernel/replicant/linux/zImage-dtb:kernel \
+
+##########
+# Lights #
+##########
+
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.samsung \
 
 ##########
 # Memory #
@@ -151,6 +138,23 @@ PRODUCT_PACKAGES += \
     android.hidl.memory@1.0-impl \
     android.hidl.memory@1.0-service \
 
+#########
+# Power #
+#########
+
+# HAL packages
+PRODUCT_PACKAGES += \
+	android.hardware.power-service.example \
+
+############
+# Security #
+############
+# Keymaster HAL packages
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-service.software \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service \
+
 ###########
 # Sensors #
 ###########
@@ -159,6 +163,40 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
 
+###########################
+# Unsupported HW features #
+###########################
+
+# Copy list of unsupported HW features
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/unsupported_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/unsupported_hardware.xml
+
+#######
+# USB #
+#######
+
+# HAL packages
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
+# Enable Hardware compoments on the framework level
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
+
+############
+# Vibrator #
+############
+
+# HAL packages
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+
+########
+# VNDK #
+########
+PRODUCT_PACKAGES += \
+    vndk_package
+
 ######
 # VR #
 ######
@@ -166,35 +204,3 @@ PRODUCT_PACKAGES += \
 # HAL packages
 PRODUCT_PACKAGES += \
     android.hardware.vr@1.0-impl \
-#########
-# Debug #
-#########
-
-# Get root on the serial console for -eng builds
-# This can help debugging early boot issues
-ifeq ($(TARGET_BUILD_VARIANT),eng)
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/console.rc:system/etc/init/console.rc
-endif
-
-###############
-# ADB support #
-###############
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.service.adb.enable=1 \
-    persist.sys.usb.config=adb
-
-####################
-# Heimdall support #
-####################
-
-# Enable flashing through heimdall
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/resize2fs_partitions.sh:system/bin/resize2fs_partitions.sh
-
-#########
-# Hacks #
-#########
-
-# HACK: prevent the device to go in suspend because it's annoying during early
-# development. Remove afterward as it consume way more energy this way.
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/prevent_suspend.sh:system/bin/prevent_suspend.sh
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/prevent_suspend.rc:system/etc/init/prevent_suspend.rc
