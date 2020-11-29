892e22713825d4fc0f95cdefee220cb36e61dcce
Martin Jücker <martin.juecker@gmail.com>
2020-11-30 00:22:13 +0100

    configure bluetooth, add missing bluetooth firmware
    Change-Id: I05bcc7cd2a02d6792a72afad74d30093b3503f43

--- bluetooth/bluetooth.mk
+++ bluetooth/bluetooth.mk
@@ -1,15 +1,17 @@
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
-    
-    #frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
 
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-service.btlinux \
+    android.hardware.bluetooth@1.1-service.btlinux \
     audio.bluetooth.default \
-    
-    #android.hardware.bluetooth.audio@2.0-impl \
-    #android.hardware.bluetooth.a2dp@1.0-impl \
-    #audio.a2dp.default \
+    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.a2dp@1.0-impl \
+    audio.a2dp.default \
 
-DEVICE_MANIFEST_FILE += $(LOCAL_PATH)/bluetooth/manifest.xml
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/BCM.hcd:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/brcm/BCM.hcd \
+
+DEVICE_MANIFEST_FILE += $(LOCAL_PATH)/manifest.xml
