769462dce50624342bb4435390790b9afecfc9ce
Martin Jücker <martin.juecker@gmail.com>
2021-05-02 22:24:53 +0200

    USB: add basic usb hal and gadget hal to make PTP and MTP work
    there are some missing bits but it's ok for now
    
    Change-Id: Ia5f825eb8d05d33cfae69f75643bb1f75d6726e9
    Signed-off-by: Martin Jücker <martin.juecker@gmail.com>

--- midas.mk
+++ midas.mk
@@ -197,6 +197,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     sensors.iio \
 
+##############
+# USB Gadget #
+##############
+
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic \
+    android.hardware.usb.gadget@1.1-service-midas
+
 ###########################
 # Unsupported HW features #
 ###########################
